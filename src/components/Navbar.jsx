6352528a3e37b5139c699705dd3c9bc26f1eb204
mrchetanshimpi <mrchetanshimpi@gmail.com>
2025-04-06 01:14:54 +0530

    added animations to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = () => {
     <div className='nav-container'>
       <div className='f-nav-container'>
         <div className='logo'><p className='f-name'>get</p><p className='s-name'>details</p></div>
-        <div className='nav-elements'>Home</div>
-        <div className='nav-elements'>About</div>
-        <div className='nav-elements'>Services</div>
-        <div className='nav-elements'>Contact</div>
+        <div className='nav-elements element1'>Home</div>
+        <div className='nav-elements element2'>About</div>
+        <div className='nav-elements element3'>Services</div>
+        <div className='nav-elements element4'>Contact</div>
       </div>
       <div className='s-nav-container'>
         <button className='login-btn'>Login / Sign Up</button>
