da26638c974515037b4ba6077dc2787073c3eeb2
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-06 16:25:14 +0530

    routing added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import '../css/Navbar.css'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='nav-container'>
       <div className='f-nav-container'>
         <div className='logo'><p className='f-name'>get</p><p className='s-name'>details</p></div>
-        <div className='nav-elements element1'>Home</div>
-        <div className='nav-elements element2'>About</div>
-        <div className='nav-elements element3'>Services</div>
-        <div className='nav-elements element4'>Contact</div>
+        <Link className='nav-elements element1'>Home</Link>
+        <Link className='nav-elements element2 text-red-500'>About</Link>
+        <Link className='nav-elements element3'>Services</Link>
+        <Link className='nav-elements element4'>Contact</Link>
       </div>
       <div className='s-nav-container'>
         <button className='login-btn'>Login / Sign Up</button>
