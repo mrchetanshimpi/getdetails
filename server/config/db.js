ba11218315bd35bce39c6a0855d241755aeaee57
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-10 23:13:17 +0530

    data base connected

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
   try {
-    const db = await mongoose.connect("mongodb://localhost:27017/big");
+    const db = await mongoose.connect("mongodb://localhost:27017/bigDb");
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
--- server/index.js
+++ server/index.js
@@ -2,9 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const morgan = require("morgan");
-
+const connectDb = require("./config/db");
+require("dotenv").config("./config/.env");
 const app = express();
+connectDb();
 // middleware that must need
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+});
