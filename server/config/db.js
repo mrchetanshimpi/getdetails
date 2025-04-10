4293d0894073dee0286580cbe0305949cf18166d
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-10 23:08:56 +0530

    data base connected

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
   try {
-    const db = await mongoose.connect("mongodb://localhost:27017/");
-    console.log("mongodb connected");
-  } catch (error) {}
+    const db = await mongoose.connect("mongodb://localhost:27017/big");
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+  }
 };
