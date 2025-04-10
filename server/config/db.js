87594c9a895160de1f67284e700082e8243f470a
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-10 23:28:35 +0530

    added imp string in env file

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
   try {
-    const db = await mongoose.connect("mongodb://localhost:27017/bigDb");
+    const db = await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
