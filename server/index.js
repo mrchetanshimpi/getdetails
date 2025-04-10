01906f5e4914ed0d753d1bc12ff13bc4a3cc0762
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-10 23:01:30 +0530

    update backend

--- server/index.js
+++ server/index.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+// middleware that must need
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
