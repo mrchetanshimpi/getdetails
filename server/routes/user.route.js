c9722aafecede20384b25ce906ae9cea6a6045cf
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-10 23:32:47 +0530

    added middleware

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { userLogin, userRegister } = require("../controllers/user.controller");
+const isAuthenticated = require("../middlewares/isAuthenticated");
 const router = express.Router();
 
-router.route("/login").post(userLogin);
-router.route("/register").post(userRegister);
+router.route("/login").post(isAuthenticated, userLogin);
+router.route("/register").post(isAuthenticated, userRegister);
 
 module.exports = router;
