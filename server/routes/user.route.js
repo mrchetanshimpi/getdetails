1e128e375f89c212fb06fbedec81012344cdcdee
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-10 23:21:24 +0530

    add routes

---
+++ server/routes/user.route.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const { userLogin, userRegister } = require("../controllers/user.controller");
+const router = express.Router();
+
+router.route("/login").post(userLogin);
+router.route("/register").post(userRegister);
