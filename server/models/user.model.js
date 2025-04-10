9077a2d6bac015ceb86f209bc7781c85d41da4ad
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-10 22:55:27 +0530

    added new schema

---
+++ server/models/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+    default:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbyjxU3kooUmOl2yKymxCFBpfrWqdyfStdZA&s",
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
