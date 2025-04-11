84eccacaaf2abc61a4107ccfd6a407adb6d75bc6
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-11 23:34:23 +0530

    added login ui

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -12,11 +12,19 @@ const Login = () => {
             </div>
             <div className='border border-green-400 text-center my-5'>
               <div>
-                <input type="text" className='rounded-lg p-2 bg-purple-300 border-none outline-none w-1/2' placeholder='enter your username'/>
+                <input type="text" className='rounded-lg p-2 bg-purple-300 border-none outline-none w-1/2' placeholder='enter your username' />
               </div>
-              <div>
-                <input type="text" className='rounded-lg ' />
+              <div className='my-4'>
+                <input type="text" className='rounded-lg p-2 bg-purple-300 border-none outline-none w-1/2' placeholder='enter your password' />
               </div>
+            </div>
+            <button className='py-2 px-4 bg-purple-400 rounded-md'>Login Now</button>
+            <div>
+              <p className='border-l border-l-gray-50'><span className='font-semibold'>Login</span> with others</p>
+            </div>
+            <div>
+              <button className='w-1/2 border inline-block p-2 my-2 bg-purple-400 rounded-md'>Login with google</button><br />
+              <button className='w-1/2 border p-2 bg-purple-400 rounded-md'>Login with Facebook</button>
             </div>
           </div>
           <div className='w-1/2'>kjk</div>
