f7aa50eecb006d09ed5cd0b8a2cc9d00f0258225
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-11 23:40:56 +0530

    authorization completed

--- server/middlewares/isAuthenticated.js
+++ server/middlewares/isAuthenticated.js
@@ -1,5 +1,20 @@
+const jwt = require("jsonwebtoken");
 const isAuthenticated = (req, res, next) => {
-  next();
+  try {
+    const authHeader = req.headers["authorization"];
+    if (authHeader) {
+      const token = authHeader.split(" ")[1]; //bearer token coming from here
+      if (token) {
+        // here compare jwt token
+
+        return next();
+      }
+    }
+    return res.status(401).json({ message: "Unauthorized: Token missing" });
+  } catch (error) {
+    console.error("Auth Middleware Error:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 module.exports = isAuthenticated;
