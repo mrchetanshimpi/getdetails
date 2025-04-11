70bfe3dc485799c1944023829cbc7746c3ab82a0
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-11 23:19:35 +0530

    added login ui

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,9 +4,11 @@ const Login = () => {
   return (
     <div>
       <div className='h-screen border border-red-700 flex items-center justify-center'>
-        <div className='border border-blue-600 h-96 w-4xl'>
-          <div>jhk</div>
-          <div>kjk</div>
+        <div className='border border-blue-600 h-[550px] w-4xl flex'>
+          <div className='border border-yellow-400 w-1/2'>
+              
+          </div>
+          <div className='w-1/2'>kjk</div>
         </div>
       </div>
     </div>
